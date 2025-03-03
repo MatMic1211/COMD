6c90426792eca6c8d2ac23e1f3f7de405baa74cd
MatMic1211 <mat.micz@wp.pl>
2025-03-03 12:09:09 +0100

    COMD: Movie service added

---
+++ Pointman.COMD/Pointman.COMD/src/app/Services/movie-service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, forkJoin } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+
+interface Movie {
+  Title: string;
+  Year: string;
+  Runtime: string;
+  Genre: string;
+  Director: string;
+  Plot: string;
+  Poster: string;
+  imdbID: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  private apiUrl = 'https://www.omdbapi.com/';
+  private apiKey = '1c4a41da';
+
+  constructor(private http: HttpClient) { }
+
+  searchMovies(searchTerm: string): Observable<Movie[]> {
+    const params = new HttpParams()
+      .set('apikey', this.apiKey)
+      .set('s', searchTerm);
+
+    return this.http.get<{ Search: { imdbID: string }[] }>(this.apiUrl, { params })
+      .pipe(
+        switchMap(response => {
+          if (!response.Search) return [];
+          const movieDetailsRequests = response.Search.map(movie => this.getMovieDetails(movie.imdbID));
+          return forkJoin(movieDetailsRequests);
+        })
+      );
+  }
+
+  private getMovieDetails(imdbID: string): Observable<Movie> {
+    const params = new HttpParams()
+      .set('apikey', this.apiKey)
+      .set('i', imdbID);
+
+    return this.http.get<Movie>(this.apiUrl, { params });
+  }
+}
